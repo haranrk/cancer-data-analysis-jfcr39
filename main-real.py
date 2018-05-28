7e420639c049ddca37d9ebad0c6a50a2e36d2cac
haranrk <haranrajkumar97@gmail.com>
2018-05-28 17:55:36 +0900

    Perfectly working NMF

--- main-real.py
+++ main-real.py
@@ -1,18 +1,28 @@
 from lib.functions import *
 from lib.NmfClass import *
 from matplotlib import pyplot as plt
+import nimfa as nf
 
-x_ori = pd.read_csv('data/input_CCLE_drug_IC50_zero-one.csv', index_col=0, header=0)
-x = clean_df(x_ori).as_matrix()
+# x_ori = pd.read_csv('data/input_CCLE_drug_IC50_zero-one.csv', index_col=0, header=0)
+# x = clean_df(x_ori).as_matrix()
+# k_list = [30, 25, 10, 3, 40,2,5,6,40,]
 
-m = NmfModel(x, 20, 250, 50)
-print(m.error)
+x = nf.examples.medulloblastoma.read(normalize=True)
+k_list = list(range(2, 10))
 
+k_list.sort()
 
+for k in k_list:
+    m = NmfModel(x, k, niter=10, super_niter=5)
+    m.super_wrapper(verbose=1)
+    print("K = %i Error = %f" % (k, m.error))
+    plt.subplot(121)
+    plt.suptitle("k = %s" % k)
 
+    heatmap(m.consensus_matrix_h)
+    plt.title("H")
+    plt.subplot(122)
 
-m.super_wrapper()
-print(m.error)
-
-heatmap(m.consensus_matrix_h)
-heatmap(m.consensus_matrix_w)
+    heatmap(m.consensus_matrix_w)
+    plt.title("W")
+    plt.show()
